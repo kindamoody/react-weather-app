f486b1aca94c94762598e428ce15cf2cf142b254
kindamoody <93117888+kindamoody@users.noreply.github.com>
2023-02-25 14:26:35 -0600

    debugging test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import WeatherSearch from "./WeatherSearch";
-import axios from "axios";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
