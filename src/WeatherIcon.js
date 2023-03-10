286ecceecaa52a06e2fc254c7b9ede1a37856d8d
kindamoody <93117888+kindamoody@users.noreply.github.com>
2023-03-10 16:25:20 -0600

    css designing

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="#280e63"
+      color="#000"
       size={props.size}
       animate={true}
     />
