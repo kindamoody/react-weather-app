9507d607a002f3163369e57d862eb64b83c18691
kindamoody <93117888+kindamoody@users.noreply.github.com>
2023-03-10 19:02:02 -0600

    fixed unused variable

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,6 +1,5 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
-import WeatherForecast from "./WeatherForecast.css";
 
 export default function WeatherForecastDay(props) {
   function maxTemperature() {
